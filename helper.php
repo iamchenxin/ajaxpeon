82ed5fb085829006f8f52ef4340b41c9dec86e90
iamchenxin <iamchenxin@gmail.com>
2015-03-30 00:25:20 +0800

    test build 6

--- helper.php
+++ helper.php
@@ -143,7 +143,7 @@ class helper_plugin_ajaxpeon extends DokuWiki_Plugin
         chdir($pagedir);
         fwrite($RRRord,"chdir($pagedir); getcwd()~~: ".getcwd()."\n");
 
-        fwrite($RRRord,"get_file_list scandir["$pagedir."/book/]\n");
+        fwrite($RRRord,"get_file_list scandir[".$pagedir."/book/]\n");
         $RRRts = scandir($pagedir."/book/");
         fwrite($RRRord,json_encode($RRRts)."\n");
         $bookdir_ls = $this->get_file_list($pagedir."/book/");
